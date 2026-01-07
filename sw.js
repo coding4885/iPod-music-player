e74c93c0081dee56acafbb667ce48968b78e19ab
coding4885 <dlwnsqja0602@gmail.com>
2026-01-07 14:30:46 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,18 +1,14 @@
-const CACHE_NAME = "ipod-pwa-v1";
-const FILES = [
-  "./",
-  "./iPod.html",
-  "./manifest.json"
-];
-
-self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES))
-  );
+const CACHE="ipod-v2";
+self.addEventListener("install",e=>{
+ e.waitUntil(
+  caches.open(CACHE).then(c=>c.addAll([
+    "./iPod.html",
+    "./manifest.json"
+  ]))
+ );
 });
-
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request))
-  );
+self.addEventListener("fetch",e=>{
+ e.respondWith(
+   caches.match(e.request).then(r=>r||fetch(e.request))
+ );
 });
